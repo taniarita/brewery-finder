aee1529d014b298d1952928bf36f9d16ba3db1c4
tania rita <TaniaRita.Mendes-ext@ab-Inbev.com>
2022-09-21 20:56:26 -0300

    Splash

--- app/src/main/java/com/example/breweryfinder/MainActivity.kt
+++ app/src/main/java/com/example/breweryfinder/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.breweryfinder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.breweryfinder.home.HomeActivity
+import java.sql.Time
+import java.util.*
+import kotlin.concurrent.schedule
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var time = Timer()
+
+    //region lifecycle methods
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash)
+        setContentView(R.layout.activity_main)
+
+        this.initialize()
+    }
+
+    override fun onPause() {
+        time.cancel()
+        super.onPause()
+    }
+    //endregion
+
+    private fun initialize() {
+        time.schedule(3000) {
+            openHome()
+        }
+    }
+
+    private fun openHome() {
+        var intent = Intent(this@MainActivity, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
